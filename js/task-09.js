723de1a64ab74b02bbf0f1beddad032ad808c4f0
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-01 19:29:19 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -6,4 +6,11 @@ console.log(handleBtnEl);
 const colorSpanEl = document.querySelector('.color');
 console.log(colorSpanEl);
 
-colorSpanEl.textContent = 'sfdsdfgsdf'
+handleBtnEl.addEventListener('click', () => {
+  // ! Колір записую у нову змінну
+  let newBodyBckgrndClr = getRandomHexColor();
+  // ! Перезапис фонового кольору
+  document.body.style.backgroundColor = newBodyBckgrndClr;
+  // ! перезапис span text
+  colorSpanEl.textContent = `${newBodyBckgrndClr}`;
+});

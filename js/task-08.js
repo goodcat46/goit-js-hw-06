18452539b019e491e61a99153bb088cc2de35af1
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-09-01 19:12:41 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const logInFormEl = document.querySelector('.login-form');
+// console.log(logInFormEl);
+/*
+? Управління формою
+*/ 
 
+logInFormEl.addEventListener('submit', event => {
+  const inputEmail = logInFormEl.elements[0];
+  // console.log(inputEmail);
+  const inputPassword = logInFormEl.elements[1];
+  // console.log(inputPassword);
+  event.preventDefault();
+
+  if (inputEmail.value.length && inputPassword.value.length !== 0) {
+    const userLoginData = {
+      [inputEmail.name]: inputEmail.value,
+      password: inputPassword.value,
+    };
+
+    console.log(userLoginData);
+    alert('Submited');
+    event.target.reset();
+    return;
+  }
+  alert('Заповніть усі поля');
+  return;
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const handleBtnEl = document.querySelector('.change-color');
+console.log(handleBtnEl);
+const colorSpanEl = document.querySelector('.color');
+console.log(colorSpanEl);
+
+colorSpanEl.textContent = 'sfdsdfgsdf'
